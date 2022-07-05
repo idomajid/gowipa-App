5fb44b645ceabdcc641aeb3176dea1e43185786f
IDO Majid <idomajid99@gmail.com>
2022-07-05 22:01:04 +0700

    added CostumFont

--- App.jsx
+++ App.jsx
@@ -1,7 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 import Tab from "./navigation/Tab";
+import Apploading from "expo-app-loading";
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    "Josefin-Sans-Light": require("./assets/fonts/JosefinSans-Light.ttf"),
+    "Josefin-Sans-Regular": require("./assets/fonts/JosefinSans-Regular.ttf"),
+    "Josefin-Sans-Bold": require("./assets/fonts/JosefinSans-SemiBold.ttf"),
+  });
+
+  if (!fontLoaded) {
+    return <Apploading />;
+  }
   return (
     <NavigationContainer>
       <Tab />
