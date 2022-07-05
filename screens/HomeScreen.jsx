de3e6a697391d173ae9eef7b9710723349b75a9a
IDO Majid <idomajid99@gmail.com>
2022-07-05 15:07:31 +0700

    adding Carousel feature

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,23 +1,68 @@
-import { StyleSheet, Text, View, Dimensions, Button } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
+import Carousel from "react-native-reanimated-carousel";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default function HomeScreen({ navigation }) {
-  const dimensions = Dimensions.get("window").width;
+  const [images, setImages] = useState([
+    require("../assets/images/jumtron/jumbotron.jpg"),
+    require("../assets/images/jumtron/jumbotron.jpg"),
+    require("../assets/images/jumtron/jumbotron.jpg"),
+  ]);
 
-  console.log(dimensions);
   return (
     <View style={styles.container}>
-      <Text>HomeScreen</Text>
-      <Button title="Items" onPress={() => navigation.navigate("Items")} />
+      <View style={styles.navbar}>
+        <View>
+          <Text>Gowipa</Text>
+        </View>
+        <TouchableOpacity>
+          <Text>Click me</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Carousel
+          autoPlay={true}
+          autoPlayInterval={2000}
+          scrollAnimationDuration={3000}
+          width={windowWidth}
+          height={windowHeight / 3.2}
+          data={images}
+          renderItem={({ item, index }) => (
+            <Image style={styles.imageJumbotron} source={item} key={index} />
+          )}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
+    width: windowWidth,
+  },
+  navbar: {
+    width: windowWidth,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    height: 60,
+    paddingTop: 20,
+    paddingHorizontal: 20,
+  },
+  imageJumbotron: {
+    width: windowWidth,
+    height: windowHeight / 3.2,
   },
 });
