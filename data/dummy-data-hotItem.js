0186a953c964fb46aca7b9333f330bd8927209cf
IDO Majid <idomajid99@gmail.com>
2022-07-06 16:58:38 +0700

    dummy-data

---
+++ data/dummy-data-hotItem.js
@@ -0,0 +1,36 @@
+module.exports = [
+  {
+    id: "001",
+    title: "Nike Bubble Gum",
+    desConclution: "Nike shoes signed by Michael Jordan",
+    description: "",
+    price: 7000,
+    photo: require("../assets/images/sportApp/whiteDomPinkShoes.jpg"),
+  },
+  {
+    id: "002",
+    title: "Vans Red Berry",
+    desConclution: "Vans limited edition signed by Elon musk",
+    description: "",
+    price: 20000,
+    photo: require("../assets/images/sportApp/redVans.jpg"),
+  },
+  {
+    id: "003",
+    title: "Random Shoes",
+    desConclution:
+      "Shoes mystery box, We will give you a random good quality shoes",
+    description: "",
+    price: 40,
+    photo: require("../assets/images/sportApp/whiteDomPinkShoes.jpg"),
+  },
+
+  {
+    id: "004",
+    title: "Second Item",
+    desConclution: "lorem imsum emet dalang kudul",
+    description: "",
+    price: 0,
+    photo: require("../assets/images/sportApp/redVans.jpg"),
+  },
+];
