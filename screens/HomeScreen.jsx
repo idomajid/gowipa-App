d8bb3a1536dcd11ad5ce2297bd3af919d6b3ee03
IDO Majid <idomajid99@gmail.com>
2022-07-06 17:38:15 +0700

    adding Flatlist for Hotitems

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -5,10 +5,13 @@ import {
   Dimensions,
   Image,
   TouchableOpacity,
+  FlatList,
 } from "react-native";
 import { useState } from "react";
 import Carousel from "react-native-reanimated-carousel";
 import ProductCard from "../components/productCard";
+
+import DummyData from "../data/dummy-data-hotItem";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
@@ -19,7 +22,7 @@ export default function HomeScreen({ navigation }) {
     require("../assets/images/jumtron/jumbotron.jpg"),
     require("../assets/images/jumtron/jumbotron.jpg"),
   ]);
-
+  console.log(DummyData);
   return (
     <View style={styles.container}>
       <View style={styles.navbar}>
@@ -44,18 +47,37 @@ export default function HomeScreen({ navigation }) {
           )}
         />
       </View>
+      <View style={{ marginTop: 10, marginHorizontal: 10 }}>
+        <Text style={styles.HotItemTitle}>Hot Items</Text>
+      </View>
       <View style={{ margin: 10, flexDirection: "row" }}>
-        <ProductCard
+        <FlatList
+          data={DummyData}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => {
+            console.log(item.photo);
+            return (
+              <ProductCard
+                productImage={item.photo}
+                title={item.title}
+                description={item.desConclution}
+                price={`$${item.price}`}
+              />
+            );
+          }}
+        />
+        {/* <ProductCard
           imageProduct={require("../assets/images/sportApp/whiteDomPinkShoes.jpg")}
           title="Nike"
           description="Nike Phoenix has a kobe autograph"
           price={`$${600}`}
         />
         <ProductCard
+          imageProduct={require("../assets/images/sportApp/redVans.jpg")}
           title="Nike"
           description="Nike Phoenix has a kobe autograph"
           price={`$${600}`}
-        />
+        /> */}
       </View>
     </View>
   );
@@ -65,6 +87,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#ffff",
     width: windowWidth,
+    height: windowHeight - 60,
   },
   navbar: {
     width: windowWidth,
@@ -78,5 +101,11 @@ const styles = StyleSheet.create({
   imageJumbotron: {
     width: windowWidth,
     height: windowHeight / 3.2,
+  },
+  HotItemTitle: {
+    fontFamily: "Josefin-Sans-Regular",
+    fontSize: 14,
+    lineHeight: 14,
+    color: "#404040",
   },
 });
