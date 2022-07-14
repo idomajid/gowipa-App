c2339612f8a72f23c8fa86948e58c18265a85bc7
IDO Majid <idomajid99@gmail.com>
2022-07-14 16:19:17 +0700

    addiing headerTransparent at ItemScreen

--- navigation/Tab.jsx
+++ navigation/Tab.jsx
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { View, Image } from "react-native";
+import { View, Image, Button, Pressable } from "react-native";
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -13,11 +14,12 @@ import HeartIcon from "../assets/tabIcons/heart.svg";
 import HomeIcon from "../assets/tabIcons/home.svg";
 import SearchIcon from "../assets/tabIcons/search.svg";
 import UserIcon from "../assets/tabIcons/user.svg";
+import ArrowBackIcon from "../assets/icons/arrowBack.svg";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-const MyHomeTabScreen = () => {
+const MyHomeTabScreen = ({ navigation }) => {
   return (
     <Stack.Navigator>
       <Stack.Screen
@@ -28,7 +30,35 @@ const MyHomeTabScreen = () => {
       <Stack.Screen
         name="Items"
         component={ItemScreen}
-        options={{ headerShown: false }}
+        options={() => ({
+          headerTitle: "",
+          headerTransparent: true,
+          headerLeft: () => (
+            <Pressable onPress={() => navigation.goBack()}>
+              <View
+                style={{
+                  width: 40,
+                  height: 40,
+                  backgroundColor: "#0D1821",
+                  borderRadius: 200,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <ArrowBackIcon width={25} height={25} fill={"#F6F9FF"} />
+              </View>
+            </Pressable>
+          ),
+          headerRight: ({ focused }) => (
+            <Pressable onPress={() => console.log("its worked")}>
+              <HeartIcon
+                width={25}
+                height={25}
+                fill={focused ? "#F6F9FF" : "#A9A9A9"}
+              />
+            </Pressable>
+          ),
+        })}
       />
     </Stack.Navigator>
   );
@@ -50,7 +80,12 @@ export default function Tabs() {
       <Tab.Screen
         name="HomeTab"
         component={MyHomeTabScreen}
-        options={{
+        options={({ route }) => ({
+          tabBarStyle: {
+            display: tabBarVisibility(route),
+            backgroundColor: "#0D1821",
+            height: 60,
+          },
           tabBarIcon: ({ focused }) => (
             <HomeIcon
               width={30}
@@ -58,7 +93,7 @@ export default function Tabs() {
               fill={focused ? "#F6F9FF" : "#A9A9A9"}
             />
           ),
-        }}
+        })}
       />
       <Tab.Screen
         name="SearchTab"
@@ -104,3 +139,12 @@ export default function Tabs() {
     </Tab.Navigator>
   );
 }
+
+const tabBarVisibility = (route) => {
+  const routeName = getFocusedRouteNameFromRoute(route) ?? "Feed";
+
+  if (routeName == "Items") {
+    return "none";
+  }
+  return "flex";
+};
