1ad8998963397730ebbdde0d19c98fdf13ffb80d
IDO Majid <idomajid99@gmail.com>
2022-07-19 07:25:41 +0700

    renaming a dummy-data

---
+++ data/dummy-data.js
@@ -0,0 +1,41 @@
+module.exports = [
+  {
+    id: "001",
+    title: "Nike Bubble Gum",
+    desConclution: "Nike shoes signed by Michael Jordan",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rutrum rhoncus semper. Fusce justo mi, tincidunt dictum posuere et, porta nec nisi. Ut sed facilisis ligula. Fusce vel ante vitae nibh blandit tristique in rutrum purus. Nullam tempus neque elit. Nullam cursus nibh turpis, eu consectetur quam volutpat eget. Cras eget sem sit amet diam faucibus volutpat. Morbi vestibulum nunc vitae lectus bibendum, ac vestibulum est suscipit. Donec at ultrices risus. Nunc dolor elit, tempor ac bibendum ut, posuere eu sem. Vivamus pretium bibendum gravida. Nullam tempus egestas libero, nec condimentum arcu convallis eu. Morbi at turpis in ante ullamcorper imperdiet. Nunc volutpat felis non elit faucibus, blandit congue augue auctor. Fusce et odio lorem. Maecenas sem risus, vehicula et porttitor quis, ultrices fringilla enim. Suspendisse eu turpis hendrerit, efficitur sapien quis, imperdiet lorem. Cras viverra, diam id sodales ultrices, augue lacus efficitur quam, ut pellentesque nisl odio eget nunc. Vestibulum vestibulum luctus metus, fringilla laoreet odio hendrerit id. Ut nisi lacus, scelerisque vel nisi id, imperdiet dapibus purus. Integer nibh risus, viverra sit amet leo blandit, venenatis lobortis justo. Curabitur vitae nisl orci. Phasellus in enim id lacus rutrum pulvinar a et risus. Duis felis arcu, placerat at vehicula nec, blandit in sem. Nullam sit amet erat diam. Suspendisse eget auctor quam, nec fermentum augue. Sed dictum, purus non pulvinar laoreet, lacus urna cursus ante, et maximus sapien nulla ut nulla. Donec gravida quam id suscipit varius. Praesent condimentum justo dui, euismod feugiat enim vulputate id. Praesent quis arcu efficitur, ultricies nibh eget, imperdiet arcu. Donec tempor tempus elementum.",
+    // description:
+    //   "Get the best shoes for you. Lists of the best shoes for men and women, including best shoes for men over 40, best shoes for tall men, and best dress shoes.",
+    price: 7000,
+    photo: require("../assets/images/sportApp/whiteDomPinkShoes.jpg"),
+  },
+  {
+    id: "002",
+    title: "Vans Red Berry",
+    desConclution: "Vans limited edition signed by Elon musk",
+    description:
+      "A unique and simple design,  limited editions vans shoes, signed by Elon Musk ",
+    price: 20000,
+    photo: require("../assets/images/sportApp/redVans.jpg"),
+  },
+  {
+    id: "003",
+    title: "Shoes mystery box",
+    desConclution: " We will give you a random good quality shoes",
+    description:
+      "Shoes lottery, find a good quality original shoes. We don't accept return or refund. except for damaged shoes ",
+    price: 40,
+    photo: require("../assets/images/sportApp/whiteDomPinkShoes.jpg"),
+  },
+
+  {
+    id: "004",
+    title: "Elevania Sneakers",
+    desConclution: "affordble high quality shoes.",
+    description:
+      "Shoes should be as beautiful as they are functional. Our team of experts has put together a guide to help you select the best for you and your feet.",
+    price: 30,
+    photo: require("../assets/images/sportApp/pair-of-shoes.jpg"),
+  },
+];
