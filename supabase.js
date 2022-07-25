5955e03a5d5abe8ddbf4b46bd175bbcb47a65511
IDO Majid <idomajid99@gmail.com>
2022-07-25 13:35:06 +0700

    adiing a supabase conflig

---
+++ supabase.js
@@ -0,0 +1,13 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_URL, SUPABASE_KEY } from "@env";
+
+const supabaseUrl = SUPABASE_URL;
+const supabaseAnonKey = SUPABASE_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  localStorage: AsyncStorage,
+  autoRefreshToken: true,
+  persistSession: true,
+  detectSessionInUrl: false,
+});
