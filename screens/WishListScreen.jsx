0fa8629d31805da15f643e359a856dc7854bf4f2
IDO Majid <idomajid99@gmail.com>
2022-09-07 17:20:28 +0700

    WishlistScreen update Test

--- screens/WishListScreen.jsx
+++ screens/WishListScreen.jsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import WishlistCard from "../components/assetCards/reausableCard";
 
 export default function WishListScreen() {
   return (
     <View style={styles.container}>
-      <Text>WishListScreen</Text>
+      <WishlistCard />
     </View>
   );
 }
