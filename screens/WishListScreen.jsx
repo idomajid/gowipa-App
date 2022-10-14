276ed0faa9e9417fbe42732036a8c5bb11264ab9
IDO Majid <idomajid99@gmail.com>
2022-10-14 19:43:08 +0700

    dummyCard for wishList screen

--- screens/WishListScreen.jsx
+++ screens/WishListScreen.jsx
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, Dimensions, View, ScrollView } from "react-native";
 import React from "react";
 import WishlistCard from "../components/assetCards/reausableCard";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default function WishListScreen() {
   return (
     <View style={styles.container}>
-      <WishlistCard />
+      <ScrollView style={{ marginVertical: 10 }}>
+        <WishlistCard />
+        <WishlistCard />
+        <WishlistCard />
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +20,7 @@ export default function WishListScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#ffff",
     alignItems: "center",
-    justifyContent: "center",
   },
 });
