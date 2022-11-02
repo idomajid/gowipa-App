01a864aeffe706f9be5d670b79c58a9db9e10acb
IDO Majid <idomajid99@gmail.com>
2022-11-02 20:04:54 +0700

    convert component to props.children

--- components/assetCards/CostumAlertCard.jsx
+++ components/assetCards/CostumAlertCard.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from "react";
-import { Alert, Modal, StyleSheet, Text, Pressable, View } from "react-native";
+import { Modal, StyleSheet, Text, Pressable, View } from "react-native";
 
 export default function CostumAlertCard(props) {
-  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={styles.centeredView}>
       <Modal
@@ -11,17 +9,7 @@ export default function CostumAlertCard(props) {
         visible={props.visible}
         onRequestClose={props.onRequestClose}
       >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={props.onPressHideModal}
-            >
-              <Text style={styles.textStyle}>Hide Modal</Text>
-            </Pressable>
-          </View>
-        </View>
+        {props.children}
       </Modal>
     </View>
   );
@@ -32,40 +20,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22,
-    backgroundColor: "rgba(52, 52, 52, 0.8)",
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
 
-  buttonClose: {
-    backgroundColor: "#2196F3",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
+    backgroundColor: "rgba(52, 52, 52, 0.8)",
   },
 });
