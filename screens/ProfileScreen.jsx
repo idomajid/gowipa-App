737c9d68ec55d4db43ef0c1cbd6d9c56a76d2a22
IDO Majid <idomajid99@gmail.com>
2022-11-14 15:04:05 +0700

    adding tshirt icon

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,38 +1,51 @@
 import { StyleSheet, Text, View, Dimensions, Pressable } from "react-native";
 import FacebookIcon from "../assets/icons/social-media/facebook.svg";
 import GoogleIcon from "../assets/icons/social-media/google.svg";
+import TshirtIcon from "../assets/icons/t-shirt.svg";
 import React from "react";
 
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.LoginTextLabel}>Login Into your account</Text>
-      <View View style={styles.circle}>
-        <Pressable onPress={() => console.log("worked Login Facebook")}>
-          <View style={[styles.mainButton, styles.addCartButton]}>
-            <View style={{ paddingRight: 10 }}>
-              <FacebookIcon width={25} height={25} fill="#404040" />
-            </View>
+      <View>
+        <Text style={styles.LoginTextLabel}>Login Into your account</Text>
+      </View>
 
-            <Text
-              style={[styles.buttonTextLabel, styles.buttonBlackLabelColor]}
-            >
-              Login with Facebook
-            </Text>
-          </View>
-        </Pressable>
-        <Pressable onPress={() => console.log("worked Login Facebook")}>
-          <View style={[styles.mainButton, styles.addCartButton]}>
-            <View style={{ paddingRight: 10 }}>
-              <GoogleIcon width={25} height={25} fill="#404040" />
+      <View style={styles.circle}>
+        <View style={{ alignItems: "center", marginBottom: 100 }}>
+          <Pressable onPress={() => console.log("worked Login Facebook")}>
+            <View style={[styles.mainButton, styles.addCartButton]}>
+              <View style={{ paddingRight: 10 }}>
+                <FacebookIcon width={25} height={25} fill="#404040" />
+              </View>
+
+              <Text
+                style={[styles.buttonTextLabel, styles.buttonBlackLabelColor]}
+              >
+                Login with Facebook
+              </Text>
             </View>
-            <Text
-              style={[styles.buttonTextLabel, styles.buttonBlackLabelColor]}
-            >
-              Login with Google
-            </Text>
-          </View>
-        </Pressable>
+          </Pressable>
+          <Pressable onPress={() => console.log("worked Login Google")}>
+            <View style={[styles.mainButton, styles.addCartButton]}>
+              <View style={{ paddingRight: 10 }}>
+                <GoogleIcon width={25} height={25} fill="#404040" />
+              </View>
+              <Text
+                style={[styles.buttonTextLabel, styles.buttonBlackLabelColor]}
+              >
+                Login with Google
+              </Text>
+            </View>
+          </Pressable>
+        </View>
+        <View
+          style={{
+            transform: [{ rotate: "35deg" }],
+          }}
+        >
+          <TshirtIcon width={200} height={200} fill="#404040" />
+        </View>
       </View>
     </View>
   );
@@ -56,7 +69,6 @@ const styles = StyleSheet.create({
     width: Dimensions.get("window").width * 1.2,
     height: Dimensions.get("window").width * 1.2,
     backgroundColor: "#EAEAEA",
-    alignItems: "center",
   },
   mainButton: {
     marginHorizontal: 20,
@@ -85,5 +97,7 @@ const styles = StyleSheet.create({
     fontFamily: "Josefin-Sans-Regular",
     fontSize: 22,
     lineHeight: 22,
+    zIndex: 3,
+    elevation: 3,
   },
 });
