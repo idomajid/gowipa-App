facad661a71c4a0b3a1d97eb5ed8717d4dfd1b83
IDO Majid <idomajid99@gmail.com>
2022-11-15 16:38:16 +0700

    windowWidth to cartScreen

--- screens/items/cartScreen.jsx
+++ screens/items/cartScreen.jsx
@@ -1,5 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import React from "react";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default function CartScreen() {
   return (
@@ -11,6 +14,7 @@ export default function CartScreen() {
 
 const styles = StyleSheet.create({
   container: {
+    width: windowWidth,
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
