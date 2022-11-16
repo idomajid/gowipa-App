339e83a3a3272003ab621044f52c48d6fa96d8a9
IDO Majid <idomajid99@gmail.com>
2022-11-16 16:45:54 +0700

    configuration settingBtn for reausable component

--- components/assetBtns/SettingBtn.jsx
+++ components/assetBtns/SettingBtn.jsx
@@ -1,14 +1,40 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Dimensions, Pressable } from "react-native";
+import SignRightIcon from "../../assets/icons/arrowRight.svg";
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
-const SettingBtn = () => {
+export default function SettingBtn({ title, onPress }) {
   return (
-    <View>
-      <Text>SettingBtn</Text>
+    <View style={styles.container}>
+      <Pressable onPress={onPress}>
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>{title}</Text>
+
+          <SignRightIcon width={25} height={25} fill="black" />
+        </View>
+      </Pressable>
     </View>
   );
-};
+}
 
-export default SettingBtn;
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    borderBottomColor: "#C7C7C7",
+    borderBottomWidth: 1,
+    backgroundColor: "#fff",
+    width: windowWidth,
+  },
+  btn: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    alignItems: "center",
+  },
+  btnText: {
+    fontFamily: "Josefin-Sans-Regular",
+    fontSize: 16,
+    lineHeight: 16,
+    color: "#404040",
+  },
+});
