e18c768785248cbd813d20db2ab6cd9d8000a172
IDO Majid <idomajid99@gmail.com>
2022-11-16 16:51:46 +0700

    adding a newScreen

---
+++ screens/profile/OrderItemScreen.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function OrderItemScreen() {
+  return (
+    <View>
+      <Text>OrderItemScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
