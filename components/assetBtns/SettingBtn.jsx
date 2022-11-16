cf634c2325f080a5b4f0e0fcc6332a3acbc87fd1
IDO Majid <idomajid99@gmail.com>
2022-11-16 15:21:06 +0700

    adding settingBtn component

---
+++ components/assetBtns/SettingBtn.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SettingBtn = () => {
+  return (
+    <View>
+      <Text>SettingBtn</Text>
+    </View>
+  );
+};
+
+export default SettingBtn;
+
+const styles = StyleSheet.create({});
