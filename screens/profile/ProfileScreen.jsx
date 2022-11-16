e9734d6c0860b07af5556652fef69dc74369852c
IDO Majid <idomajid99@gmail.com>
2022-11-16 15:21:48 +0700

    adding imageBackground

--- screens/profile/ProfileScreen.jsx
+++ screens/profile/ProfileScreen.jsx
@@ -1,12 +1,45 @@
-import { View, Text } from "react-native";
-import React from "react";
-
-const ProfileScreen = () => {
+import { ImageBackground, View, Text, StyleSheet } from "react-native";
+import SettingBtn from "../../components/assetBtns/SettingBtn";
+export default function ProfileScreen() {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../../assets/images/jumtron/ProfileCover.jpg")}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Text style={styles.imageText}>Jane Miss Doe</Text>
+      </ImageBackground>
+      <View style={{ marginVertical: 10 }}>
+        <Text style={styles.AccountLabel}>Account</Text>
+      </View>
+      <SettingBtn />
     </View>
   );
-};
+}
 
-export default ProfileScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  image: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: 140,
+  },
+  imageText: {
+    fontFamily: "Josefin-Sans-Regular",
+    fontSize: 24,
+    lineHeight: 24,
+    color: "#404040",
+  },
+  AccountLabel: {
+    fontFamily: "Josefin-Sans-Regular",
+    fontSize: 14,
+    lineHeight: 14,
+    color: "#404040",
+  },
+});
