ecfc66f0534e3a14937909f94c02dfdc7b272c38
IDO Majid <idomajid99@gmail.com>
2022-12-13 18:24:07 +0700

    fixing a blankPage

--- screens/items/cartScreen.jsx
+++ screens/items/cartScreen.jsx
@@ -12,12 +12,14 @@ import { useState, useEffect, useCallback } from "react";
 import CartCard from "../../components/assetCards/ReausableCard";
 import NumberPick from "../../components/numberPick";
 import { supabase } from "../../supabase";
+import CartIcon from "../../assets/icons/shoppingCart.svg";
 
 const windowWidth = Dimensions.get("window").width;
 
 export default function CartScreen() {
   const [cartItem, setCartItem] = useState(null);
   const [checkoutId, setCheckoutId] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [orderBy, setOrderBy] = useState("created_at");
 
@@ -28,6 +30,7 @@ export default function CartScreen() {
   // };
 
   useEffect(() => {
+    setIsLoading(true);
     async function cartFetch() {
       let { data, error } = await supabase
         .from("product_cart")
@@ -39,9 +42,11 @@ export default function CartScreen() {
       setCartItem(data);
     }
     cartFetch();
+    setIsLoading(false);
   }, [cartItem]);
 
   const handleDelete = async () => {
+    setIsLoading(true);
     const { data: products, error } = await supabase
       .from("product_cart")
       .delete()
@@ -55,6 +60,7 @@ export default function CartScreen() {
     if (products) {
       console.log(products);
     }
+    setIsLoading(false);
   };
 
   // const deleteCart = async () => {
@@ -65,53 +71,69 @@ export default function CartScreen() {
 
   return (
     <View style={styles.container}>
-      <View style={{ marginTop: 60 }}>
-        <FlatList
-          data={cartItem}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => {
-            return (
-              <CartCard
-                title={item.title_product}
-                price={item.price_product}
-                imageUrl={item.imageUrl_product}
-              >
-                <NumberPick
-                  numberPickLayout={styles.numberPickLayout}
-                  styleContainer={styles.styleContainer}
-                  title="Size"
-                  styleFont={styles.styleFont}
-                  inputValue={item.size_product}
-                />
-                <NumberPick
-                  numberPickLayout={styles.numberPickLayout}
-                  styleContainer={styles.styleContainer}
-                  title="Quantity"
-                  styleFont={styles.styleFont}
-                  inputValue={1}
-                />
-                <TouchableOpacity activeOpacity={0.4} onPress={handleDelete}>
-                  <View style={styles.buttonContainer}>
-                    <Text style={styles.buttonText}>Delete Item</Text>
-                  </View>
-                </TouchableOpacity>
-              </CartCard>
-            );
+      {cartItem == undefined ? (
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
           }}
-        />
-
-        <View style={{ marginBottom: 30, marginTop: 10 }}>
-          <Pressable onPress={() => console.log("Disable")}>
-            <View style={[styles.buyNowButton, styles.mainButton]}>
-              <Text
-                style={[styles.buttonTextLabel, styles.buttonWhiteLabelColor]}
-              >
-                Proceed to checkout
-              </Text>
-            </View>
-          </Pressable>
+        >
+          <CartIcon width={270} height={270} fill="#eaeaea" />
+          <View>
+            <Text style={styles.labelText}>
+              Your cart is empty, let us help you to find something your like
+            </Text>
+          </View>
         </View>
-      </View>
+      ) : (
+        <View style={{ marginTop: 60 }}>
+          <FlatList
+            data={cartItem}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => {
+              return (
+                <CartCard
+                  title={item.title_product}
+                  price={item.price_product}
+                  imageUrl={item.imageUrl_product}
+                >
+                  <NumberPick
+                    numberPickLayout={styles.numberPickLayout}
+                    styleContainer={styles.styleContainer}
+                    title="Size"
+                    styleFont={styles.styleFont}
+                    inputValue={item.size_product}
+                  />
+                  <NumberPick
+                    numberPickLayout={styles.numberPickLayout}
+                    styleContainer={styles.styleContainer}
+                    title="Quantity"
+                    styleFont={styles.styleFont}
+                    inputValue={1}
+                  />
+                  <TouchableOpacity activeOpacity={0.4} onPress={handleDelete}>
+                    <View style={styles.buttonContainer}>
+                      <Text style={styles.buttonText}>Delete Item</Text>
+                    </View>
+                  </TouchableOpacity>
+                </CartCard>
+              );
+            }}
+          />
+
+          <View style={{ marginBottom: 30, marginTop: 10 }}>
+            <Pressable onPress={() => console.log("Disable")}>
+              <View style={[styles.buyNowButton, styles.mainButton]}>
+                <Text
+                  style={[styles.buttonTextLabel, styles.buttonWhiteLabelColor]}
+                >
+                  Proceed to checkout
+                </Text>
+              </View>
+            </Pressable>
+          </View>
+        </View>
+      )}
     </View>
   );
 }
@@ -119,7 +141,7 @@ export default function CartScreen() {
 const styles = StyleSheet.create({
   container: {
     width: windowWidth,
-
+    justifyContent: "center",
     flex: 1,
     alignItems: "center",
     backgroundColor: "#fff",
@@ -172,5 +194,11 @@ const styles = StyleSheet.create({
   },
   buttonWhiteLabelColor: {
     color: "#BFBFBF",
+  },
+  labelText: {
+    fontFamily: "Josefin-Sans-Regular",
+    fontSize: 16,
+    lineHeight: 16,
+    color: "#c7c7c7",
   },
 });
